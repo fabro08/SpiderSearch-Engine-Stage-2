e9ab070e83c9b23393884abb80ac880b9160022e
fabr08 <fabriciomiranda55@gmail.com>
2015-05-18 13:55:54 -0600

    ARBOL BST

--- src/Estructuras/Main.java
+++ src/Estructuras/Main.java
@@ -15,7 +15,12 @@ public class Main {
 //		arbolito.insertar("Casa",3);
 //		arbolito.insertar("Estuvo",8);
 //		arbolito.inOrden(arbolito.obtenerRaiz());
-		//if (elimina == true)
+//		arbolito.postOrden(arbolito.obtenerRaiz());
+//		//if (elimina == true)
+		BST aBst = new BST();
+		aBst.insertarNombre("afafafa!!");
+		System.out.println(aBst.obtenerNombre());
+				
 			//javax.swing.JOptionPane.showMessageDialog(null, "Se elimino el nodo");
 //		else {
 	//		javax.swing.JOptionPane.showMessageDialog(null, "No se encontro el nodo");
@@ -24,24 +29,25 @@ public class Main {
 		
 		//System.out.println("");
 		//arbolito.inOrden(arbolito.obtenerRaiz());
-		XMLParser URL1 = new XMLParser("configuracion.xml","URL");
+	//	XMLParser URL1 = new XMLParser("configuracion.xml","URL1");
 		//XMLParser URL2 = new XMLParser("configuracion.xml","URL2");
 		//XMLParser URL3 = new XMLParser("configuracion.xml","URL3");
 		
-		URL1.obtenerCola();
-		//System.out.println(URL1.getArchivo());
-		//System.out.println(URL1.getPeso());
-				
-				
-		
-		String a= String.valueOf(URL1.obtenerCola());
-		System.out.println(a.getTexto());
+//		URL1.obtenerCola();
+//		
+//		System.out.println(URL1.getArchivo());
+//		//System.out.println(URL1.getPeso());
+//				
+//		System.out.println(URL1.obtenerCola());	
+//		
+//		String a= String.valueOf(URL1.obtenerCola());
+//		//System.out.println(a);
 //TikaParser  aParser = new TikaParser(a);
-		//System.out.println(aParser.GetHandler());
-	//	String b= String.valueOf(aParser.GetHandler());
-		
-	//Separar fghSeparar = new Separar(b);
-	//fghSeparar.CicloToken();
+////System.out.println(aParser.GetHandler());
+//		//String b= String.valueOf(aParser.GetHandler());
+//		
+//	//Separar fghSeparar = new Separar(b);
+//	//fghSeparar.CicloToken();
 
 	
 	
